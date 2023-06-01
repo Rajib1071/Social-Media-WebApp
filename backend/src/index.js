9c9cec4225f0e8d567b8b0e4ffa1e60782cd2dd2
Rajib1071 <rajibmondal20192020@gmail.com>
2023-06-01 14:29:07 +0530

    mongodb creds secured

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,23 +3,30 @@ const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
 const postRoutes = require('./routes/postRoutes');
 const morgan = require('morgan');
+const dotenv = require('dotenv');
+// Load environment variables from .env file
+dotenv.config();
 
 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
 // Connect to the database
-mongoose.connect('mongodb+srv://rajibmondal20192020:OKaOebXf85mdSrPm@my-social-media-app-clu.9ei26xp.mongodb.net/?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+console.log(process.env.MONGODB_URL)
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log('Connected to the database');
   })
   .catch((error) => {
     console.error('Failed to connect to the database:', error);
   });
+
 
 // Add middleware for parsing JSON and handling URL-encoded data
 app.use(express.json());
