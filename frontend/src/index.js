5800da95551b4ca621ddb291380ec28e031254c9
Rajib1071 <rajibmondal20192020@gmail.com>
2023-09-06 09:57:43 +0530

    commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import { useAppContext, AppProvider } from './AppContext'; // Import the AppProvider
 
-
+// "start": "react-scripts start",
 const root = createRoot(document.getElementById('root'));
 root.render(<AppProvider>
     <App />
